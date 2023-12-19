7f57dd2e749a2589947ca94aa6e89c7628482097
faymanns <florian.aymanns@t-online.de>
2023-12-19 15:57:45 +0100

    initial commit

---
+++ src/napari_bee_annotator/__init__.py
@@ -0,0 +1,20 @@
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+from ._reader import napari_get_reader
+from ._widget import (
+    ExampleQWidget,
+    ImageThreshold,
+    threshold_autogenerate_widget,
+    threshold_magic_widget,
+)
+
+__all__ = (
+    "napari_get_reader",
+    "ExampleQWidget",
+    "ImageThreshold",
+    "threshold_autogenerate_widget",
+    "threshold_magic_widget",
+)
